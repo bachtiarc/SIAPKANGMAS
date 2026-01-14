c873753423344f2bd2d10b7712c606821cd2561f
Cikal Wahyuning Bachtiar <cikalwahyuning19@gmail.com>
2026-01-14 10:44:39 +0700

    updated

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -51,7 +51,7 @@ class ResetPasswordController extends Controller
         );
 
         return $status === Password::PASSWORD_RESET
-            ? redirect()->route('login')->with('status', 'Password Anda berhasil direset! Silakan login.')
+            ? redirect()->route('login')->with('status', 'Password Anda berhasil di reset! Silakan login.')
             : back()->withErrors(['email' => 'Link reset password tidak valid atau sudah kadaluarsa.']);
     }
 }
